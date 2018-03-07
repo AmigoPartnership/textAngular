2516f7f8816ce668bd05908a26a743e86c30e3bb
James Clarke <jamesraymondclarke@gmail.com>
2018-03-07 12:06:18 +0000

    Text Sanitize

--- dist/textAngular.umd.js
+++ dist/textAngular.umd.js
@@ -2823,7 +2823,7 @@ angular.module('textAngular.taBind', ['textAngular.factories', 'textAngular.DOM'
             var _isReadonly = false;
             var _focussed = false;
             var _skipRender = false;
-            var _disableSanitizer = attrs.taUnsafeSanitizer || taOptions.disableSanitizer;
+            var _disableSanitizer = true;
             var _keepStyles = attrs.taKeepStyles || taOptions.keepStyles;
             var _lastKey;
             // see http://www.javascripter.net/faq/keycodes.htm for good information
